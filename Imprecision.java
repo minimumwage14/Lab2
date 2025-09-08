4bf3e3d9b706414227262be16b5964269758e736
Jacob Antonio <jacobantonio0814@gmail.com>
2025-09-07 21:09:14 -0700

    y,z

--- Imprecision.java
+++ Imprecision.java
@@ -4,17 +4,19 @@
  * We will be using a large number for x and showing how x/x is close but not equal to 1
  *
  * @author Jacob Antonio
- * @version 1.0
+ * @version 1.1
  * @since 9/7/2025
  */
 public class Imprecision
 { 
     public static void main(String[]args){
         double x = 12345.6789e200;
-        double y = 1/x;
-        double z = x*y;
+        double y,z;
+        y= 1/x;
+        z = x*y;
         System.out.println(" The value of x is " + x);
         System.out.println(" The value of y is " + y);
         System.out.println(" The value of z is " + z);
+        System.out.println ("The value of 1-z is: " + (1-z));
 }
 }
